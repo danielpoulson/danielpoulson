f79957412a377808da436cf0b96038c604d25e8f
Daniel Poulson <danielp@deltalabs.com.au>
2020-03-13 10:06:59 +1100

    kates poem

--- server.js
+++ server.js
@@ -6,19 +6,23 @@
  */
 
 // modules
-var static = require( 'node-static' ),
-    port = 8080,
-    http = require( 'http' );
+var static = require('node-static'),
+  port = process.env.PORT || 5000,
+  http = require('http')
 
 // config
-var file = new static.Server( './public', {
-    cache: 3600,
-    gzip: true
-} );
+var file = new static.Server('./public', {
+  cache: 3600,
+  gzip: true
+})
 
 // serve
-http.createServer( function ( request, response ) {
-    request.addListener( 'end', function () {
-        file.serve( request, response );
-    } ).resume();
-} ).listen( port );
+http
+  .createServer(function(request, response) {
+    request
+      .addListener('end', function() {
+        file.serve(request, response)
+      })
+      .resume()
+  })
+  .listen(port)
